ac85589091c2a1fe278d5be6ecd126fa3eb1a5fa
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-05 18:12:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
+const App = ()=> {
+    let [one , setOne] = useState(false);
+    let [two , setTwo] = useState(false);
+    let [three , setThree] = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <div>
+        <h2>Child Component</h2>
+        <ul>
+          <li>Learn React
+            {one || 
+              <button onClick={()=>setOne(true)}>Complete</button>
+            }
+          </li>
+          <li>Build a React app
+            {two || 
+              <button onClick={()=>setTwo(true)}>Complete</button>
+            }
+          </li>
+          <li>Deploy a React app
+            {three || 
+              <button onClick={()=>setThree(true)}>Complete</button>
+            }
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
